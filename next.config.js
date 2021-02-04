eeca829b0a7d683e18118d46e85e930a44216449
Nitin Pradeep Kumar <getnpk@gmail.com>
2021-02-04 18:29:47 +0900

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,7 +1,31 @@
 // https://github.com/vercel/next.js/issues/11584
-const withAntdLess = require('next-plugin-antd-less');
+const withAntdLess = require('next-plugin-antd-less')
 
-module.exports = (phase, { defaultConfig }) => withAntdLess({
- ...defaultConfig,
-  lessVarsFilePath: './styles/antd-custom.less',
-});
+module.exports = (phase, { defaultConfig }) =>
+  withAntdLess({
+    ...defaultConfig,
+    lessVarsFilePath: './styles/antd-custom.less',
+    webpack: (config, { isServer }) => {
+      if (isServer) {
+        const antStyles = /antd\/.*?\/style.*?/
+        const origExternals = [...config.externals]
+        config.externals = [
+          (context, request, callback) => {
+            if (request.match(antStyles)) return callback()
+            if (typeof origExternals[0] === 'function') {
+              origExternals[0](context, request, callback)
+            } else {
+              callback()
+            }
+          },
+          ...(typeof origExternals[0] === 'function' ? [] : origExternals),
+        ]
+
+        config.module.rules.unshift({
+          test: antStyles,
+          use: 'null-loader',
+        })
+      }
+      return config
+    },
+  })
