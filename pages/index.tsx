5a0f328a800ee26930c070bcbeef354d5c48226a
Nitin Pradeep Kumar <nitinpradeep.kumar@oyolife.co.jp>
2021-02-08 18:14:01 +0900

    Remove api route dep

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,18 +2,8 @@ import React from 'react'
 import Head from 'next/head'
 import styles from '../styles/Home.module.scss'
 import FormDemo from '../components/FormDemo'
-import useSWR from 'swr'
-import Person from '../components/Person'
-import { Row, Col } from 'antd'
-
-const fetcher = (url) => fetch(url).then((res) => res.json())
 
 export default function Home() {
-  const { data, error } = useSWR('/api/people', fetcher)
-
-  if (error) return <div>Failed to load</div>
-  if (!data) return <div>Loading...</div>
-
   return (
     <div className={styles.container}>
       <Head>
@@ -23,14 +13,6 @@ export default function Home() {
 
       <main className={styles.main}>
         <FormDemo />
-
-        <Row>
-          {data.map((p, i) => (
-            <Col span={6} key={i}>
-              <Person person={p} />
-            </Col>
-          ))}
-        </Row>
       </main>
     </div>
   )
