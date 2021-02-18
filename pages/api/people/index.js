7aa8547bf87250ed31dc1f94073f5376fbb5392b
Nitin Pradeep Kumar <nitinpradeep.kumar@oyolife.co.jp>
2021-02-18 11:27:37 +0900

    Test SWR

--- pages/api/people/index.js
+++ pages/api/people/index.js
@@ -1,5 +1,28 @@
+import Amplify, { withSSRContext } from 'aws-amplify'
 import { people } from '../../../data'
 
-export default function handler(req, res) {
-  res.status(200).json(people)
+// Amplify SSR configuration needs to be enabled within each API route
+Amplify.configure({
+  Auth: {
+    // REQUIRED - Amazon Cognito Region
+    region: 'ap-northeast-1',
+
+    // OPTIONAL - Amazon Cognito User Pool ID
+    userPoolId: 'ap-northeast-1_0NCoHSIU4',
+
+    // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+    userPoolWebClientId: '2hb06d27ehja2v463qcl9km9e',
+  },
+  ssr: true,
+})
+
+export default async function handler(req, res) {
+  const { Auth } = withSSRContext({ req })
+  try {
+    const user = await Auth.currentAuthenticatedUser()
+    res.json({ people })
+  } catch (err) {
+    res.statusCode = 401
+    res.json({ error: err })
+  }
 }
--- pages/profile.js
+++ pages/profile.js
@@ -1,10 +1,18 @@
 import { useState, useEffect } from 'react'
 import { Auth } from 'aws-amplify'
 import { useRouter } from 'next/router'
+import useSWR from 'swr'
+import Person from '../components/Person'
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json())
 
 const Profile = () => {
   const [user, setUser] = useState(null)
   const router = useRouter()
+
+  const { data, error } = useSWR('/api/people', fetcher)
+  if (error) return <div>failed to load</div>
+
   useEffect(() => {
     Auth.currentAuthenticatedUser()
       .then((user) => setUser(user))
@@ -21,6 +29,18 @@ const Profile = () => {
         <>
           <h1>Welcome, {user.signInUserSession.idToken.payload.email}</h1>
           <h2>Welcome, {user.signInUserSession.idToken.payload['cognito:groups']}</h2>
+
+          <div>
+            {data && data.people ? (
+              data.people.map((person) => (
+                <div key={person.name}>
+                  <Person person={person} />
+                </div>
+              ))
+            ) : (
+              <div>Loading...</div>
+            )}
+          </div>
         </>
       )}
     </div>
