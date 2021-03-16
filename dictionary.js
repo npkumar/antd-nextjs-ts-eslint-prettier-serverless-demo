9f3bcdc92a26b288a77438e4a8aa0e4ecfa8126e
Nitin Pradeep Kumar <nitinpradeep.kumar@oyolife.co.jp>
2021-03-16 18:32:57 +0900

    Remove amplify dir

--- dictionary.js
+++ dictionary.js
@@ -2,7 +2,7 @@ import { Translations } from '@aws-amplify/ui-components'
 
 // https://github.com/aws-amplify/amplify-js/blob/main/packages/amplify-ui-components/src/common/Translations.ts
 const vocabularies = {
-  [Translations.SIGN_UP_HEADER_TEXT]: 'Custom 新しいアカウントを作成する',
+  [Translations.SIGN_UP_HEADER_TEXT]: '新しいアカウントを作成する',
   [Translations.CREATE_ACCOUNT_TEXT]: 'アカウントの作成',
   [Translations.SIGN_UP_SUBMIT_BUTTON_TEXT]: 'アカウントの作成',
   [Translations.EMAIL_LABEL]: 'メールアドレス *',
@@ -13,7 +13,7 @@ const vocabularies = {
   [Translations.RESET_PASSWORD_TEXT]: 'パスワードを変更する',
   [Translations.SIGN_IN_ACTION]: 'ログイン',
   [Translations.SIGN_IN_TEXT]: 'ログイン',
-  [Translations.SIGN_IN_HEADER_TEXT]: 'Custom ログイン',
+  [Translations.SIGN_IN_HEADER_TEXT]: 'ログイン',
   [Translations.SIGN_UP_HAVE_ACCOUNT_TEXT]: 'アカウントを持っています？',
   [Translations.NO_ACCOUNT_TEXT]: 'アカウントテキストなし',
 }
