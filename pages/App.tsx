a64018d68cfc6e5eb43c632619e25e316597138a
Nitin Pradeep Kumar <nitinpradeep.kumar@oyolife.co.jp>
2021-03-17 09:58:53 +0900

    Simplify congnito

--- pages/App.tsx
+++ pages/App.tsx
@@ -20,19 +20,13 @@ import {
 import { appWithTranslation } from 'next-i18next';
 import jaJP from 'antd/lib/locale/ja_JP';
 import enGB from 'antd/lib/locale/en_GB';
+import awsCongnitoConfig from '../amplify/config';
 
 const { Header, Sider, Content } = Layout;
 
 Amplify.I18n.putVocabulariesForLanguage('ja', dictionary);
 
-Amplify.configure({
-  aws_project_region: process.env.NEXT_PUBLIC_AWS_PROJECT_REGION,
-  aws_cognito_identity_pool_id: process.env.NEXT_PUBLIC_AWS_USER_POOLS_ID,
-  aws_cognito_region: process.env.NEXT_PUBLIC_AWS_COGNITO_REGION,
-  aws_user_pools_id: process.env.NEXT_PUBLIC_AWS_USER_POOLS_ID,
-  aws_user_pools_web_client_id: process.env.NEXT_PUBLIC_AWS_USER_POOLS_WEB_CLIENT_ID,
-  ssr: true,
-});
+Amplify.configure({ ...awsCongnitoConfig });
 
 const MyApp = ({ Component, pageProps }) => {
   const [authState, setAuthState] = useState(AuthState.SignedOut);
